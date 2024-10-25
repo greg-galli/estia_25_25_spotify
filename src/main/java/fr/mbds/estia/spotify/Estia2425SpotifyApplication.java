d24ccde585bc642dfc4a11ac574e849d7ac19e20
Gregory Galli <greg.galli@gmail.com>
2024-10-25 16:00:58 +0200

    Added some code to the main to create some data at startup

--- src/main/java/fr/mbds/estia/spotify/Estia2425SpotifyApplication.java
+++ src/main/java/fr/mbds/estia/spotify/Estia2425SpotifyApplication.java
@@ -1,7 +1,13 @@
 package fr.mbds.estia.spotify;
 
+import fr.mbds.estia.spotify.entities.Music;
+import fr.mbds.estia.spotify.repositories.MusicRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Estia2425SpotifyApplication {
@@ -9,5 +15,23 @@ public class Estia2425SpotifyApplication {
     public static void main(String[] args) {
         SpringApplication.run(Estia2425SpotifyApplication.class, args);
     }
+
+
+    @Bean
+    CommandLineRunner start(MusicRepository musicRepository) {
+        return args -> {
+            List<String> musicTitles = List.of("Music Title 1", "Music Title 2", "Music Title 3");
+            // Iterate over the list of music titles
+            musicTitles.forEach(musicTitle -> {
+                Music musicInstance = Music.builder()
+                        .title(musicTitle)
+                        .trackLength(20)
+                        .genre("Music Genre")
+                        .build();
+                musicRepository.save(musicInstance);
+            });
+        };
+    }
+
 
 }
